ccc717dee537d153d8127224408e7b3f0e02b685
Matheus Sampaio <matheuschagas415@gmail.com>
2023-11-15 12:10:30 -0300

    Add class Connections in project

---
+++ src/main/java/br/com/medvirtua/java_global/controller/CorsFilter.java
@@ -0,0 +1,22 @@
+package br.com.medvirtua.java_global.controller;
+
+import java.io.IOException;
+
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.ext.Provider;
+
+@Provider
+public class CorsFilter implements ContainerResponseFilter{
+
+    @Override
+    public void filter(ContainerRequestContext request,
+                       ContainerResponseContext response) throws IOException {
+        response.getHeaders().add("Access-Control-Allow-Origin", "*");
+        response.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+        response.getHeaders().add("Access-Control-Allow-Credentials", "true");
+        response.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+    }
+
+}
